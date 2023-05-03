e74a2f16084484f005b0ce43b7a196010e18e21d
Александр Озимлевский <alex@MacBook-Air-Aleksandr.local>
2023-05-03 19:35:32 +0300

    origin

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//  Carousel
+
+ $(document).ready(function () {
+  $(".carousel").slick({
+   dots: false,
+   arrows: false,
+  });
+  });
+  
+//Burger
+
+let burger = document.querySelector("#burger-icon");
+let header = document.querySelector("#header");
+
+burger.onclick = function () {
+  header.classList.toggle("menu-open");
+};
