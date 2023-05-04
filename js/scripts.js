3ce764c7df417b4b67c8432a9a1fd889900644e9
Александр Озимлевский <alex@MacBook-Air-Aleksandr.local>
2023-05-04 17:59:29 +0300

    changed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 //  Carousel
 
- $(document).ready(function () {
+$(document).ready(function () {
   $(".carousel").slick({
-   dots: false,
-   arrows: false,
+    dots: false,
+    arrows: false,
+    fade: true,
+    speed: 500,
+    cssEase: "linear",
+    autoplay: true,
+    autoplaySpeed: 4000,
   });
-  });
-  
+});
+
 //Burger
 
 let burger = document.querySelector("#burger-icon");
@@ -15,3 +20,26 @@ let header = document.querySelector("#header");
 burger.onclick = function () {
   header.classList.toggle("menu-open");
 };
+
+/* Counter */
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+
+    const increment = target / 250;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCounter();
+});
